5524e5c4a575de23686a751d6b1e698d525cb215
JackyDom <sopheaoudom8@gmail.com>
2025-06-19 15:43:31 +0700

    updating

--- run_waitress.py
+++ run_waitress.py
@@ -2,5 +2,5 @@ from waitress import serve
 from app import app  # your Flask app in app.py
 
 if __name__ == '__main__':
-    serve(app, host='127.0.0.1', port=5000)
+    serve(app, host='0.0.0.0', port=5000)
     # app.run(debug=True)
